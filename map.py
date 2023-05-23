bfc440ef789209f76cf277f63f945fe878505f26
lrobyr <laly.robyr@epfl.ch>
2023-05-23 17:17:58 +0200

    code occupency map simu

---
+++ map.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+map = np.zeros((int((max_x-min_x)/res_pos), int((max_y-min_y)/res_pos))) # 0 = unknown, 1 = free, -1 = occupied
+
+def occupancy_map(sensor_data):
+    global map, t
+    pos_x = sensor_data['x_global']
+    pos_y = sensor_data['y_global']
+    yaw = sensor_data['yaw']
+    
+    for j in range(4): # 4 sensors
+        yaw_sensor = yaw + j*np.pi/2 #yaw positive is counter clockwise
+        if j == 0:
+            measurement = sensor_data['range_front']
+        elif j == 1:
+            measurement = sensor_data['range_left']
+        elif j == 2:
+            measurement = sensor_data['range_back']
+        elif j == 3:
+            measurement = sensor_data['range_right']
+        
+        for i in range(int(range_max/res_pos)): # range is 2 meters
+            dist = i*res_pos
+            idx_x = int(np.round((pos_x - min_x + dist*np.cos(yaw_sensor))/res_pos,0))
+            idx_y = int(np.round((pos_y - min_y + dist*np.sin(yaw_sensor))/res_pos,0))
+
+            # make sure the point is within the map
+            if idx_x < 0 or idx_x >= map.shape[0] or idx_y < 0 or idx_y >= map.shape[1] or dist > range_max:
+                break
+
+            # update the map
+            if dist < measurement:
+                map[idx_x, idx_y] += conf
+            else:
+                map[idx_x, idx_y] -= conf
+                break
+    
+    map = np.clip(map, -1, 1) # certainty can never be more than 100%
+
+    # only plot every Nth time step (comment out if not needed)
+    if t % 50 == 0:
+        plt.imshow(np.flip(map,1), vmin=-1, vmax=1, cmap='gray', origin='lower') # flip the map to match the coordinate system
+        plt.savefig("map.png")
+    t +=1
+
+    return map
